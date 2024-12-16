2c784ba743c03028e912d856a7376be22b8d7ebc
wangzhihai <wangzhihai1@yonyou.com>
2024-12-16 21:14:25 +0800

    init

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入本地文件名或者不输入等待Socket输入:");
+        System.out.println("请输入本地文件名或者输入回车接受Socket输入:");
         String filename = scanner.nextLine();
         scanner.close();
         if (filename.length() > 4) {
